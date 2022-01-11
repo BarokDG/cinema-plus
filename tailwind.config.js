c5f0bab07c6f68e8ea7c309e92180c21f0b73613
nati-m <it.natnael.mekonnen@gmail.com>
2022-01-11 21:35:15 +0300

    switched to tailwind postcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["**/*.php", "*.php"],
+  content: ["**/*.html", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#efcf4f',
+        secondary: '#c25934',
+        tertiary: '#0c4b65',
+        tertiaryDark: '#0a3f55',
+      },
+      fontFamily: {
+          spicy: ['Spicy Rice'],
+          sans: ['Open Sans']
+      }
+    },
   },
   plugins: [],
 };
