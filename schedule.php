a313a39496cd01e64d06d76aa1b3697408d50d7d
jrermi12 <it.ermias.asmare@gmail.com>
2022-01-12 23:03:23 +0300

    finished design layout for schedule and details page

---
+++ schedule.php
@@ -0,0 +1,109 @@
+<div class="py-24 max-w-screen-lg mx-auto">
+        <div class="text-center mb-16">
+          <p class="mt-4 text-sm leading-7 text-gray-500 font-regular">
+              EVENT
+            </p>
+            <h3 class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
+              Event<span class="text-indigo-600"> Scheduling</span>
+            </h3>
+        </div>
+        
+        <div class=" px-5 py-3 uppercase text-sm text-gray-500 font-medium">
+          <ul class="flex justify-center md:justify-start">
+            <li class="text-sm md:text-xl text-yellow-500"> <a href="">Mon</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500"> <a href="">Tue</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500"> <a href="">Wed</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500"> <a href="">Thu</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500"> <a href="">Fri</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500"> <a href="">Sat</a></li>
+            <li class="ml-8 text-sm md:text-xl text-yellow-500 "> <a href="">Sun</a></li>
+          </ul>
+        </div>
+        
+        <hr>
+       
+          <div class="overflow-hidden mt-2 py-6 md:flex gap-4">
+            
+              <img src="movie-7-270x340.jpg" class="wp-full h-70 ml-10 md:justify-start md:h-48 object-cover rounded-lg" alt="">
+           
+            <div class="card m-4">
+              <span class="text-yellow-400 text-lg font-semibold">Thriller, Horrer</span>
+              <span class="text-gray-900 block text-xl font-bold pt-2"> Daylight</span>
+              <span class="text-gray-500 block text-sm">Lorem ipsum, dolor sit amet consectetur Lorem ipsum, 
+                dolor sit amet consectetur Lorem ipsum, 
+                dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+                dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+              </span>
+  
+              <button class="text-yellow-500 pt-3 text-sm">FULL SYNOPSIS ></button> 
+              
+              <p class="text-gray-500 text-xs pt-4">VIEWING TIMES</p>
+              <div class=" flex flex-row pt-3">
+                <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">12:15</button>
+                <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">4:15</button>
+                <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">6:15</button>
+                <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+            
+                <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+  
+              </div>
+            </div> 
+          </div>    
+        <hr/>
+        <div class="overflow-hidden mt-2 py-6 md:flex gap-4">
+            
+          <img src="movie-7-270x340.jpg" class="wp-full h-70 ml-10 md:justify-start md:h-48 object-cover rounded-lg" alt="">
+       
+        <div class="card m-4">
+          <span class="text-yellow-400 text-lg font-semibold">Thriller, Horrer</span>
+          <span class="text-gray-900 block text-xl font-bold pt-2"> Daylight</span>
+          <span class="text-gray-500 block text-sm">Lorem ipsum, dolor sit amet consectetur Lorem ipsum, 
+            dolor sit amet consectetur Lorem ipsum, 
+            dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+            dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+          </span>
+
+          <button class="text-yellow-500  pt-3 text-sm">FULL SYNOPSIS ></button> 
+          
+          <p class="text-gray-500 text-xs pt-4">VIEWING TIMES</p>
+          <div class=" flex flex-row pt-3">
+            <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">12:15</button>
+            <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">4:15</button>
+            <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">6:15</button>
+            <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+        
+            <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+
+          </div>
+        </div> 
+      </div>
+      <hr>
+      
+      <div class="overflow-hidden mt-12 py-6 md:flex gap-4">
+            
+        <img src="movie-7-270x340.jpg" class="wp-full h-70 ml-10 md:justify-start md:h-48 object-cover rounded-lg" alt="">
+     
+      <div class="card m-4">
+        <span class="text-yellow-400 text-lg font-semibold">Thriller, Horrer</span>
+        <span class="text-gray-900 block text-xl font-bold pt-2"> Daylight</span>
+        <span class="text-gray-500 block text-sm">Lorem ipsum, dolor sit amet consectetur Lorem ipsum, 
+          dolor sit amet consectetur Lorem ipsum, 
+          dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+          dolor sit amet consectetur Lorem ipsum, dolor sit amet consectetur 
+        </span>
+
+        <button class="text-yellow-500  pt-3 text-sm">FULL SYNOPSIS ></button> 
+        
+        <p class="text-gray-500 text-xs pt-4">VIEWING TIMES</p>
+        <div class=" flex flex-row pt-3">
+          <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">12:15</button>
+          <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">4:15</button>
+          <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">6:15</button>
+          <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+      
+          <button class="bg-gray-300 rounded-lg py-2 px-3 ml-3">8:15</button>
+
+        </div>
+      </div> 
+    </div>
+    </div>
