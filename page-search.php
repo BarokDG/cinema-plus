59031ec27831a5481267fe57aae44d7b1b5280d0
BarokDG <it.barok.dagim@gmail.com>
2022-01-27 17:31:05 +0300

    Search form page and redirection added

---
+++ page-search.php
@@ -0,0 +1,9 @@
+<div class="">
+  <form id="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+    <label class="" for="s">Perform a New Search:</label>
+    <div class="">
+      <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
+      <input id="search-submit" type="submit" value="Search">
+    </div>
+  </form>
+</div>
