7b48e84009e6c46398f6e29f79bb4e266fdcf744
BarokDG <it.barok.dagim@gmail.com>
2022-01-27 21:25:42 +0300

    blog page styles and links to author

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -24,38 +24,42 @@
 
 
     <!-- Post Title : with link to full post -->
-    <h1 class="title">
-        <a href="<?php echo esc_url( get_permalink() )?>">
-            <?php the_title(  );?>
-        </a>
-    </h1>
 
-    <!-- The Author -->
-    <?php the_author( );?>
+    <div class="post-excerpt mx-5 md:mx-auto max-w-[75ch]">
+        <?php the_post_thumbnail( )?>
+        <div class="__content mt-2">
+            <h1 class="text-2xl font-bold">
+                <a href="<?php echo esc_url( get_permalink() )?>">
+                    <?php the_title(  );?>
+                </a>
+            </h1>
+            <p class="mt-3"><?php the_excerpt( ) ?></p>
+            <div class="__info mt-3">
+                <?php the_date(); ?> / 
+                <a class="text-red-700" href="<?php get_author_posts_url(the_author_meta('ID')) ?>"><?php the_author();?></a>
+            </div>
+        </div>
+        <div class="__footer mt-6 flex justify-between">
+            <!-- The Categories -->
+            <p>Category : <?php
+                if ( get_the_category()) {
+                    foreach( get_the_category() as $category) {
+                        echo $category->name . ' '; 
+                    }
+                }
+            ?></p>
 
-    <!-- The Date -->
-    <span><?php the_date() ?></span>
+            <!-- The tags -->
+            <p>Tags: <?php
+                if ( get_the_tags()) {
+                    foreach( get_the_tags() as $tag) {
+                        echo "<span class='inline-block px-3 mx-1 border-yellow-500 border rounded-full'>". ucfirst($tag->name) . "</span>"; 
+                    }
+                }
+            ?></p>
+        </div>
+    </div>
 
-    <!-- The Excerpt -->
-    <p><?php the_excerpt( ) ?></p>
-
-    <!-- The Categories -->
-    <p>Category : <?php
-        if ( get_the_category()) {
-            foreach( get_the_category() as $category) {
-                echo $category->name . ' '; 
-            }
-        }
-    ?></p>
-
-    <!-- The tags -->
-    <p>Tags: <?php
-        if ( get_the_tags()) {
-            foreach( get_the_tags() as $tag) {
-                echo "# " . $tag->name . ' '; 
-            }
-        }
-    ?></p>
 
     <!-- Number Of Comments -->
     <span><?php echo get_comments_number() ?></span>
--- template-parts/posts/parts/posts-author.php
+++ template-parts/posts/parts/posts-author.php
@@ -1,3 +1,4 @@
 <section id="author" class="mt-6">
-    <h3 class="text-lg mb-2">Article by <a class="text-red-700"><?php the_author_meta('display_name', get_post_field ('post_author')); ?></a></h3>
+    <?php $author_id = get_post_field ('post_author'); ?>
+    <h3 class="text-lg mb-2">Article by <a href="<?= get_author_posts_url($author_id); ?>" class="text-red-700"><?php the_author_meta('display_name', $author_id); ?></a></h3>
 </section>
