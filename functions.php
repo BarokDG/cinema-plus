c9be4b113ce8d3953d17b1174bc667ef938fc298
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-01-28 17:22:36 +0300

    Duplicate Search Create Fixed

--- functions.php
+++ functions.php
@@ -216,12 +216,19 @@ add_action( 'wp_head',"cp_customize_header_css");
             'post_author'   => 1,
     );
 
-     if(!isset($page_check->ID)){
+    $page = get_page_by_path('/search');
+
+    if(!$page){
+        if(!isset($page_check->ID)){
         $new_page_id = wp_insert_post($new_page);
         if(!empty($new_page_template)){
             update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
         }
+         }
     }
+    
+
+    
  }
 
  add_action( 'after_switch_theme', 'cp_add_search_page' );
