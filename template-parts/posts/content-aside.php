6acd303f0e7a9dc7c9876753f4f032e14d6ce1b8
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-01-28 16:14:40 +0300

    Added Font Files

--- template-parts/posts/content-aside.php
+++ template-parts/posts/content-aside.php
@@ -2,14 +2,14 @@
 <?php get_template_part("/template-parts/posts/parts/posts-header"); ?>
 
 <!-- Display Post Content -->
-<section id="content-container" class="w-full flex flex-wrap justify-between h-fit  bg-red-400">
-    <article id="content-section" class="text-justify w-fit xl:w-4/6 pt-12 px-5 bg-amber-500">
+<section id="content-container" class="w-full flex flex-wrap justify-between h-fit  ">
+    <article id="content-section" class="text-justify w-fit xl:w-4/6 pt-12 px-5 ">
         <?php get_template_part("/template-parts/posts/parts/posts-content");?>
     </article>
-    <aside id="content-aside" class="w-full bg-slate-800  xl:w-4/12 pt-12">
+    <aside id="content-aside" class="w-full bg-slate-300 xl:w-4/12 pt-12 pl-20">
         <?php 
-        if( is_active_sidebar("sidebar-one")) {
-            dynamic_sidebar("sidebar-one");
+        if( is_active_sidebar("blog-sidebar")) {
+            dynamic_sidebar("blog-sidebar");
         } ?>
     </aside>
 </section>
